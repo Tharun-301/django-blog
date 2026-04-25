dbbc01270404c48abc9fa2dfd9f696d59b47cce6
Tharun Sathunuru <tharunsathunuri373@gmail.com>
2026-04-25 10:54:27 +0530

    Add category pages, context processor, and base templates

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.http import HttpResponse
+
+from blogs.models import Blog, Category
 
 # Create your views here.
+def posts_by_category(request, category_id):
+  posts = Blog.objects.filter(status='Published', category=category_id)
+  category = get_object_or_404(Category, pk=category_id)
+  # try:
+  #   category = Category.objects.get(pk=category_id)
+  # except:
+  #     #Redirect user to home page
+  #     return redirect('home') 
+  
+  context = {
+    'posts' : posts,
+    'category': category,
+  }
+  return render(request, 'posts_by_category.html', context)
