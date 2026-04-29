a9652717639d1ffd01b4df41c3873d27033b138e
Tharun Sathunuru <tharunsathunuri373@gmail.com>
2026-04-29 11:10:10 +0530

    Created blog post CRUD in dashboard

--- blogs/models.py
+++ blogs/models.py
@@ -24,7 +24,7 @@ class Blog(models.Model):
   category = models.ForeignKey(Category, on_delete = models.CASCADE)
   author = models.ForeignKey(User, on_delete = models.CASCADE)
   featured_image = models.ImageField(upload_to = 'uploads/%Y/%m/%d')
-  short_description = models.TextField(max_length = 500)
+  short_description = models.TextField(max_length = 250)
   blog_body = models.TextField(max_length = 2000)
   status = models.CharField(max_length =20, choices=STATUS_CHOICES, default = 0)
   is_featured = models.BooleanField(default = False)
