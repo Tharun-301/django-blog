d810d2a8a1728e9f43fa2237ccb59c325f7da6d2
Tharun Sathunuru <tharunsathunuri373@gmail.com>
2026-04-30 10:14:53 +0530

    Implement comment system with validation and template rendering

--- blogs/views.py
+++ blogs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse
 
-from blogs.models import Blog, Category
+from .models import Blog, Category, Comment
 from django.db.models import Q
 
 # Create your views here.
@@ -22,8 +22,30 @@ def posts_by_category(request, category_id):
 
 def blogs(request, slug):
   single_blog = get_object_or_404(Blog, slug=slug, status='Published')
+  if request.method == 'POST':
+    if not request.user.is_authenticated:
+      return redirect('login')
+    
+    comment_text = request.POST.get('comment')
+
+    if not comment_text or comment_text.strip() == "":
+      return redirect('blogs', slug=slug)
+
+    Comment.objects.create(
+      user=request.user,
+      blog=single_blog,
+      comment=comment_text
+    )
+    return redirect('blogs', slug=slug)
+
+  # comment
+  comments = Comment.objects.filter(blog=single_blog)
+  comment_count = comments.count()
+  print('Comments=>', comments)
   context = {
-    'single_blog': single_blog
+    'single_blog': single_blog,
+    'comments':comments,
+    'comment_count':comment_count,
   }
   return render(request, 'blogs.html', context) 
 
